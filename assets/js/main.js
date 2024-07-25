647fbaabd634d21c045f442a6aeb745f4675044c
Mohammedshafi <shafiking6922@gmail.com>
2024-07-25 12:10:27 +0530

    feat:ui improvements

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
-// Write your javascript code here
+
+let showSideBarBtn = document.getElementById("burg-btn");
+let sideBar = document.getElementById("sidebar");
+
+
+showSideBarBtn.addEventListener("click",()=>{
+    if(sideBar.classList == "hidden"){
+    sideBar.classList.remove("hidden");
+    }else{
+        sideBar.classList.add("block");
+    }
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'primary':"#131842",
+        "secondry":"#ECCEAE",
+        "text":"#FBF6E2",
+        "optional":"#E68369",
+      },
+    },
+    
   },
   plugins: [],
 };
